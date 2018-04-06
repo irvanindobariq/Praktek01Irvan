a87c6ad92199aa9e75e3dbfc62c2330720d6ead1
Irvan <mawarpasiraman@gmail.com>
2018-04-06 07:03:40 +0700

    Membuat atribut dan behavior

--- src/praktek01irvan/Sepatu.java
+++ src/praktek01irvan/Sepatu.java
@@ -10,5 +10,17 @@ package praktek01irvan;
  * @author ACER-PC
  */
 public class Sepatu {
+    String Ukuran;
+    String Warna;
+    String Bahan;
+    String Merk;
+    String Harga;
     
+    void cetakInfo() {
+        System.out.println("Ukuran :"+Ukuran);
+        System.out.println("Warna :"+Warna);
+        System.out.println("Bahan :"+Bahan);
+        System.out.println("Merk :"+Merk);
+        System.out.println("Harga :"+Harga);
+    }
 }
