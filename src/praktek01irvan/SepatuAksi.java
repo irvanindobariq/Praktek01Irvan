037b0e8ef522ed6770b4e11d8b168be650fb2914
Irvan <mawarpasiraman@gmail.com>
2018-04-06 07:27:25 +0700

    Mengisi Nilai Atribut

--- src/praktek01irvan/SepatuAksi.java
+++ src/praktek01irvan/SepatuAksi.java
@@ -12,5 +12,11 @@ package praktek01irvan;
 public class SepatuAksi {
     public static void main(String[] args) {
         Sepatu Rina = new Sepatu();
+        
+        Rina.Ukuran="35";
+        Rina.Warna="Putih";
+        Rina.Bahan="Kulit";
+        Rina.Merk="Nike";
+        Rina.Harga="200000";
     }
 }
