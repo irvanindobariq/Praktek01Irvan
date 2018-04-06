717c850005fda4f9d21d7460877bf1372518d7a7
Irvan <mawarpasiraman@gmail.com>
2018-04-06 07:05:42 +0700

    Membuat class aksi dan method main

---
+++ src/praktek01irvan/SepatuAksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01irvan;
+
+/**
+ *
+ * @author ACER-PC
+ */
+public class SepatuAksi {
+    public static void main(String[] args) {
+        
+    }
+}
